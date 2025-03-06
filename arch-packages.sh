4a850044fdb1471b16672085da64dc427cc0c478
Yash Khatri <yashXkhatri@proton.me>
2025-03-06 18:14:48 +0530

    Update arch-packages.sh

--- arch-packages.sh
+++ arch-packages.sh
@@ -2,4 +2,4 @@
 sudo pacman -Syu xorg alacritty qtile ly python-psutil dunst rofi starship flameshot pipewire-pulse alsa-utils brightnessctl eza fzf \
 ttf-firacode-nerd nitrogen lxappearance blueman nm-connection-editor thunar xarchiver neovim ristretto geany rtkit qt5ct go \
 && git clone https://gitlab.com/dwt1/shell-color-scripts.git && cd shell-color-scripts && makepkg -si && cd .. && \
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -S qt5-styleplugins
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -S qt5-styleplugins && git clone https://github.com/NvChad/starter ~/.config/nvim
