f0d2c359da941e07048cb5e98805ac91741f451d
Yash Khatri <yashXkhatri@proton.me>
2025-03-06 10:25:21 +0530

    added xorg lol arch-packages.sh

--- arch-packages.sh
+++ arch-packages.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-sudo pacman -Syu alacritty qtile ly python-psutil dunst rofi starship flameshot pipewire-pulse alsa-utils brightnessctl eza fzf \
+sudo pacman -Syu xorg alacritty qtile ly python-psutil dunst rofi starship flameshot pipewire-pulse alsa-utils brightnessctl eza fzf \
 ttf-firacode-nerd nitrogen lxappearance blueman nm-connection-editor thunar xarchiver neovim ristretto geany rtkit qt5ct go \
 && git clone https://gitlab.com/dwt1/shell-color-scripts.git && cd shell-color-scripts && makepkg -si && cd .. && \
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -S qt5-styleplugins
